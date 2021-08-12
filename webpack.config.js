411195df9366303bc98263fa6b9cf4abf0f9f6b9
astradzhao <42476976+astradzhao@users.noreply.github.com>
2021-08-11 23:22:50 -0500

    hover/theme

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,16 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
+        filename: 'index_bundle.js',
+        publicPath: '/'
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'app/index.html'
         })
     ], 
-    mode: 'development'
+    mode: 'development',
+    devServer: {
+        historyApiFallback: true
+    }
 }
